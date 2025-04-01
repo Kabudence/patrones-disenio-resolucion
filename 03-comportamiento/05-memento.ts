1e8ccf9ccf48267962288f3622212b47d2555a67
Josue Hidalgo Bustamante <164217768+Kabudence@users.noreply.github.com>
2025-04-01 16:22:55 -0500

    feat:added memento pattern example solution

--- 03-comportamiento/05-memento.ts
+++ 03-comportamiento/05-memento.ts
@@ -8,3 +8,101 @@
  *
  * https://refactoring.guru/es/design-patterns/memento
  */
+
+class GameMemento{
+    private level: number;
+    private hp: number;
+    private position: string;
+    
+    constructor(level: number, hp: number, position: string) {
+        this.level = level;
+        this.hp = hp;
+        this.position = position;
+    }
+    
+    getlevel(): number {
+        return this.level;
+    }
+    getHp(): number {
+        return this.hp;
+    }
+    getPosition(): string {
+        return this.position;
+    }
+
+}
+
+class Game{
+    private level: number=1;
+    private hp: number=100;
+    private position:string ='inicio';
+
+    constructor() {
+        console.log(
+            `
+        Jugando en el nivel ${this.level}!
+        salud: ${this.hp}
+        position: ${this.position}`);
+    }
+    save(): GameMemento{
+        return new GameMemento(this.level, this.hp, this.position);
+    }
+    play(level: number, hp:number, position:string): void {
+        this.level = level;
+        this.hp = hp;
+        this.position = position;
+        console.log(`
+        Jugando en el nivel ${this.level}!
+        salud: ${this.hp}
+        position: ${this.position}`
+        );
+    }
+    restore(memento: GameMemento): void {
+        this.level=memento.getlevel()
+        this.hp=memento.getHp()
+        this.position=memento.getPosition()
+        console.log(`
+        Progreso restaurado
+        Jugando en el nivel ${this.level}!
+        salud: ${this.hp}
+        position: ${this.position}
+        `)
+        
+    }
+}
+
+class GameHistory{
+    private mementos: GameMemento[] = [];
+    
+    push(memento: GameMemento) {
+        this.mementos.push(memento)
+    }
+    pop(): GameMemento| undefined {
+        return this.mementos.pop() ?? null
+    }
+}
+
+function createGame(){
+    const game = new Game(); 
+    const history = new GameHistory();
+    history.push(game.save());
+    
+    //jugador avanza en el juego
+    
+    game.play(2,90,`bosque encantado`);
+    history.push(game.save());
+    
+    game.play(3,50,`bosque DE FUEGO`);
+    history.push(game.save());
+    
+    game.play(4,150,`bosque DE FUEGO`);
+    
+    
+    
+    console.log(`restore:`);
+    game.restore(history.pop()!)
+
+    console.log(`restore:`);
+    game.restore(history.pop()!)
+}
+createGame();
--- 03-comportamiento/05.2-memento.ts
+++ 03-comportamiento/05.2-memento.ts
@@ -8,8 +8,8 @@
  *
  * https://refactoring.guru/es/design-patterns/memento
  */
+import {COLORS} from "../helpers/colors";
 
-import { COLORS } from '../helpers/colors.ts';
 
 // Una pizarra donde se pueden agregar figuras
 
@@ -44,8 +44,7 @@ class DrawingBoard {
 
   // Crear un Memento del estado actual de la pizarra
   save(): DrawingMemento {
-    // TODO: Implementar el método save para guardar el estado actual
-    throw new Error('Method not implemented.');
+    return new DrawingMemento(this.shapes);
   }
 
   // Restaurar el estado de la pizarra desde un Memento
@@ -62,13 +61,13 @@ class History {
   // Guardar un Memento
   // TODO: Implementar push para guardar en la historia
   push(memento: DrawingMemento): void {
-    throw new Error('Method not implemented.');
+    this.mementos.push(memento);
   }
 
   // Recuperar el último Memento
   // TODO: Implementar pop para recuperar el último memento
   pop(): DrawingMemento | undefined {
-    throw new Error('Method not implemented.');
+   return this.mementos.pop()!;
   }
 }
 
